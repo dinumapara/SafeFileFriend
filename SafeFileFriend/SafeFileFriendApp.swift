f05d9f9fe7f8beec30c6f492b35560b834a4b07c
Klaus Mapara <mapara@krick.com>
2025-04-23 22:35:47 +0200

    Initial Commit

---
+++ SafeFileFriend/SafeFileFriendApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeFileFriendApp.swift
+//  SafeFileFriend
+//
+//  Created by Dinu Mapara on 23.04.25.
+//
+
+import SwiftUI
+
+@main
+struct SafeFileFriendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
